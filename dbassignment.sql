5807cb646f59b14a9a03d382c06f5467739d51fb
Jan Chylarecki <janekchylarecki@gmail.com>
2026-03-09 21:45:18 +0100

    Query 4.a added
    "Example query that operates over multiple tables via a JOIN operation"
    +
    comment in natural language

--- dbassignment.sql
+++ dbassignment.sql
@@ -199,3 +199,8 @@ SELECT * FROM Pull_Requests;
 SELECT * FROM Comments;
 
 
+-----=== QUERIES ===-----
+
+--1
+SELECT Users.username, Repositories.name AS repository_name FROM Users JOIN Repositories ON Users.user_id = Repositories.owner_user_id
+-- Joins the Users and Repositories tables. Retrieves the username of users and the names of repositories they own. The join condition matches each repository to the user who owns it.
